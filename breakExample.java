c42ff4ee3ef2e35460d466105d466502e2233070
unknown <kanon-X>
2025-04-17 22:34:04 +0600

    Added breakExample.java

---
+++ breakExample.java
@@ -0,0 +1,12 @@
+public class breakExample {
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+            if(i==6){
+                break;
+            }
+
+System.out.println(i);
+        }
+    }
+}
+
