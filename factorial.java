4da41a6748f457a8d70cc3ab65c3e3f5bf4cbeb5
unknown <kanon-X>
2025-04-17 22:38:54 +0600

    Added factorial.java

---
+++ factorial.java
@@ -0,0 +1,11 @@
+public class factorial {
+        public static void main(String[]args){
+            int n=10;
+            int factiorial=1;
+            for(int i=1;i<=n;i++){
+                factiorial=factiorial*i;
+            }
+            System.out.println(factiorial);
+        }
+    }
+
